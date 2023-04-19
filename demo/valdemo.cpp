2ecebb96811f3e27a3d97072d7b8ead22e5a808b
YonghanLI <113684407+YonghanLI@users.noreply.github.com>
2023-04-19 17:16:21 +0800

    Update valdemo.cpp

--- demo/valdemo.cpp
+++ demo/valdemo.cpp
@@ -58,17 +58,17 @@ double find_level(Function f,IntervalVector res_box){
 
 int main(int argc, const char** argv){
 
-    // Function f("x1","x2","x3","x4","x5","pow(x1-1,2)+pow(x2-2,2)+pow(x3-3,2)+pow(x4-4,2)+pow(x5-5,2)");
-    // int dimval=5;
-    // IntervalVector init_box(dimval,Interval(-10,10));
+    Function f("x1","x2","x3","x4","x5","pow(x1-1,2)+pow(x2-2,2)+pow(x3-3,2)+pow(x4-4,2)+pow(x5-5,2)");
+    int dimval=5;
+    IntervalVector init_box(dimval,Interval(-10,10));
 
-    Function f("x1","x2","x3","-pow(x1,2)-exp(x2)-pow(x3-1,2)");
-    int dimval=3;
-    IntervalVector init_box(dimval);
-    init_box[0]=Interval(-1,1);
-    init_box[1]=Interval(-3,4);
-    init_box[2]=Interval(-2,5);
-
+//     Function f("x1","x2","x3","-pow(x1,2)-exp(x2)-pow(x3-1,2)");
+//     int dimval=3;
+//     IntervalVector init_box(dimval);
+//     init_box[0]=Interval(-1,1);
+//     init_box[1]=Interval(-3,4);
+//     init_box[2]=Interval(-2,5);
+//    two examples to be chosen 
 
     IntervalVector res_box=BrunchFindBox(f,init_box,dimval);
     cout<<"res box is :"<<res_box<<endl;
